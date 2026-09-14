8ae8e628ef4a32292238a86b4ddf6ee4061d4696
clarixtechnology-sys <clarixtechnology@gmail.com>
2026-09-15 01:35:59 +0800

    Update login.py on feature branch

--- login.py
+++ login.py
@@ -2,4 +2,4 @@ def login(username, password):
     if username == "admin" and password == "1234":
         return "Authentication successful"
     else:
-        return "Authentication failed"git add login.py
+        return "Authentication failed"
